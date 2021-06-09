8e14b76a52b6c6d1d98d859c8ff559b8f6f26f46
julio <julio.a.luque@gmail.com>
2021-06-09 01:16:35 -0300

    Cambio mysql a postgres

--- src/main/java/com/example/ticket/entity/TicketEntity.java
+++ src/main/java/com/example/ticket/entity/TicketEntity.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -16,8 +17,9 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(name = "TICKET")
 public class TicketEntity {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
